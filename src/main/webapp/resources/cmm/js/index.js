299b783c7194e15afe58c470768f5909a1daaf6c
HannaChae <hannachae.dev@gmail.com>
2020-12-23 21:55:39 +0900

    22:00

--- src/main/webapp/resources/cmm/js/index.js
+++ src/main/webapp/resources/cmm/js/index.js
@@ -6,10 +6,9 @@ cmm = (() => {
 	const init = ctx => {
 		alert(`${ctx}`)
     $('.move-home').click(e => {location.href = '/'})
-	$('#manager_register').click(e => { location.href = `${ctx}/transfer/sym/mgr/register`})
-	$('#teacher_register').click(e => { location.href = `${ctx}/transfer/sym/tea/register`})	
+	$('#manager_register').click(e => { location.href = `${ctx}/admin/mgr/register`})
+	$('#teacher_register').click(e => { location.href = `${ctx}/admin/tea/register`})	
 	$(`#linkedin`).click(e=>{
-		alert(`타일즈 보러가기`)
 		 location.href = `${ctx}/home`
 	})
 	}
