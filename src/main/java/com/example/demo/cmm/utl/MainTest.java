b0c02d66300b0425cf84b5c223c519cefac60c59
HannaChae <hannachae.dev@gmail.com>
2021-01-05 22:05:35 +0900

    22:00

---
+++ src/main/java/com/example/demo/cmm/utl/MainTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.cmm.utl;
+
+
+
+public class MainTest{
+	public static void main(String[] args) {
+		DummyGenerator bs = new DummyGenerator();
+		System.out.println(bs.makeName());
+		
+	}
+}
--- src/main/java/com/example/demo/www/chap05/MappingService.java
+++ src/main/java/com/example/demo/www/chap05/MappingService.java
@@ -1,45 +1,38 @@
 package com.example.demo.www.chap05;
 
 import static java.util.stream.Collectors.toList;
-import static com.example.demo.zzz.chap04.Dish.menu;
+
 
 import java.util.Arrays;
 import java.util.List;
 
-import com.example.demo.zzz.chap04.Dish;
 
 public class MappingService{
 
-  public List<String> myMap(){
-    // map
-    List<String> dishNames = menu.stream()
-        .map(Dish::getName)
+   public List<String> myMap(){
+    return MyDish.menu.stream()
+        .map(MyDish::getName)
         .collect(toList());
-    System.out.println(dishNames);
-  }
+   }
+    public List<Integer> myMap2(){
+    	return Arrays.asList("Hello", "World").stream()
+    	        .map(String::length)
+    	        .collect(toList());
+    }
+    public List<String> myFlatMap(){
+    	return Arrays.asList("Hello", "World").stream()
+    	        .flatMap((String line) -> Arrays.stream(line.split("")))
+    	        .distinct().collect(toList());
+    }
+    public List<int[]> myFlatMap2(){
+    	List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
+        List<Integer> numbers2 = Arrays.asList(6,7,8);
+        return numbers1.stream()
+            .flatMap((Integer i) -> numbers2.stream()
+                .map((Integer j) -> new int[]{i, j})
+            )
+            .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
+            .collect(toList());
+    }
 
-    // map
-    List<String> words = Arrays.asList("Hello", "World");
-    List<Integer> wordLengths = words.stream()
-        .map(String::length)
-        .collect(toList());
-    System.out.println(wordLengths);
-
-    // flatMap
-    words.stream()
-        .flatMap((String line) -> Arrays.stream(line.split("")))
-        .distinct()
-        .forEach(System.out::println);
-
-    // flatMap
-    List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
-    List<Integer> numbers2 = Arrays.asList(6,7,8);
-    List<int[]> pairs = numbers1.stream()
-        .flatMap((Integer i) -> numbers2.stream()
-            .map((Integer j) -> new int[]{i, j})
-        )
-        .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
-        .collect(toList());
-    pairs.forEach(pair -> System.out.printf("(%d, %d)", pair[0], pair[1]));
-  }
-
+}
